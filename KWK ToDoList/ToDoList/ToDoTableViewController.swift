daab87ce6f3dcc1636484838c26d6b4d7415d73e
Molly Wysocki <82623087+MollyWysocki@users.noreply.github.com>
2021-08-02 11:57:03 -0400

    completed adding a new to do

--- KWK ToDoList/ToDoList/ToDoTableViewController.swift
+++ KWK ToDoList/ToDoList/ToDoTableViewController.swift
@@ -40,7 +40,7 @@ class ToDoTableViewController: UITableViewController {
 
    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return listOfToDo.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -60,14 +60,13 @@ class ToDoTableViewController: UITableViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let nextAddToDoVC = segue.destination as? AddToDoViewController {
+            nextAddToDoVC.previousToDoTVC = self
+        }
+       
     }
-    */
+
 
 }
